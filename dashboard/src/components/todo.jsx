d3f95a64725b0152da714b5f5883e114e4ad7564
Yvette334 <y.muhoracye@alustudent.com>
2025-10-27 22:14:04 +0200

    added

--- dashboard/src/components/todo.jsx
+++ dashboard/src/components/todo.jsx
@@ -38,18 +38,18 @@ export default function Todo() {
 
   return (
     <div className='w-full min-h-screen flex flex-col items-center bg-gray-900 text-white'>
-        <div className='border rounded-2xl m-30 w-120 shadow-gray-600'>
+        <div className='border rounded-2xl bg-gray-800 w-full max-w-md mx-auto my-8'>
         <h1 className='text-center p-8 font-bold text-5xl'>To do List</h1>
         <div className='text-center m-8 shadow-gray-600'>
             <input className='border rounded-2xl py-4 pr-4 pl-4 text-center w-full mb-3' type="text" placeholder='Enter a task' value={newTask} onChange={Input} />
-            <button className='bg-blue-500 px-4 py-4 rounded-lg w-full ' onClick={Add}>Add</button>
+            <button className='bg-blue-500 hover:bg-blue-600 px-4 py-4 rounded-lg w-full' onClick={Add}>Add</button>
         </div>
         <div className=' h-auto px-4 pb-4'>
         <ul className='w-full max-w-md space-y-3'>
             {tasks.map((task,index)=>
-                <li key={index} className='bg-gray-800 rounded-lg p-3 flex justify-between items-center cursor-grab'draggable onDragStart={(e)=> onDragStart(e, index)} onDrop={()=>dropItem (index)} onDragOver={(e)=> e.preventDefault()}>
+                <li key={index} className='border border-gray-700 bg-gray-600 rounded-lg p-3 flex justify-between items-center'draggable onDragStart={(e)=> onDragStart(e, index)} onDrop={()=>dropItem (index)} onDragOver={(e)=> e.preventDefault()}>
                 <span>{task}</span>
-                <button className='border text-red-200 bg-red-500 ' onClick={() =>Delete(index)}>Delete</button>
+                <button className='bg-red-500 hover:bg-red-600 text-white px-3 py-2' onClick={() =>Delete(index)}>Delete</button>
             </li>
             )}
         </ul>
